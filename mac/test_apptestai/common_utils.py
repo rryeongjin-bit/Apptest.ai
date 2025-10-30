1313420994d593c16fce3972003f16884ad73128
Yeongjin-heo <ridi.heo@ridi.com>
2025-10-30 09:34:15 +0900

    1030 레포폴더정리

---
+++ mac/test_apptestai/common_utils.py
@@ -0,0 +1,31 @@
+
+import os
+import pytest
+from playwright.sync_api import sync_playwright
+from playwright.sync_api import TimeoutError
+
+def scroll_until_element_found(page, target_text, step=500, max_scrolls=5):
+    """
+    지정된 셀렉터 요소가 나타날 때까지 스크롤 다운.
+    
+    :param page: playwright의 page 객체
+    :param selector: 찾을 셀렉터 (예: 'div.item:nth-child(1)')
+    :param step: 한 번에 스크롤할 픽셀 높이
+    :param max_scrolls: 최대 스크롤 시도 횟수 (무한루프 방지)
+    :return: 요소가 발견되면 True, 아니면 False
+    """
+    for i in range(max_scrolls):
+        try:
+            # 요소가 보이면 바로 리턴
+            if page.locator(target_text).is_visible():
+                print(f"✅ target_element 발견! (시도 횟수: {i+1})")
+                return True
+        except TimeoutError:
+            pass
+
+        # JS 실행으로 스크롤 다운
+        page.evaluate(f"window.scrollBy(0, {step})")
+        page.wait_for_timeout(500)  # 스크롤 후 약간의 대기 (0.5초)
+
+    print("❌ target_element 발견 실패")
+    return False
---
+++ mac/test_apptestai/element_total.py
@@ -0,0 +1,33 @@
+# -------------------------------
+# 로그인
+# -------------------------------
+account_section = "#root > header > div.sc-empnci.jgzAsH > div.sc-ePDLzJ.rrOtd > div > div > div"
+btn_changeaccount = "#root > header > div.sc-empnci.jgzAsH > div.sc-ePDLzJ.rrOtd > div > img"
+qa_account = "text=RIDI 시나리오 수정"
+
+# -------------------------------
+# mobile app 폴더
+# -------------------------------
+
+folder_mobileapp = "#root > div.sc-bJBgwP.cukmPM > div.sc-fThUAz.izgTQR > div.sc-SrznA.jxvbfe > div.sc-czkgLR.MPcHN > div:nth-child(2)"
+folder_title_section = "#content_box"
+project_title = "#content_box > div > main > div > div.sc-fTyFcS.hvAcvE > div.sc-hsUFQk.eifibf > div.sc-dNckAz.gGfvGa > div"
+
+# project_startapp : 
+# project_tabbar : 
+# project_genrehome : 
+# project_contentshome : 
+# project_contentshome_preview : 
+# project_viewer : 
+# project_usersfile : 
+# project_shorcut : 
+project_widget= "#default-table > tbody > tr:nth-child(5) > td:nth-child(1) > div > div"
+
+btn_test_run = "#content_box > div > main > div > div.sc-fDinKg.fneeqI > div > div.sc-goDuUA.bbgRRK > div.sc-libkiD.bnTBaE"
+title_recent_result = "#content_box > div > main > div > div.sc-fDinKg.fneeqI > div > div.sc-fPQgqs.dnSSIZ > div.sc-fhzFiK.khqNog.title-name"
+
+first_testrun_id = "#default-table > tbody > tr:nth-child(1) > td:nth-child(2) > a"
+testrun_id_section = "#result-wrapper > div.sc-jzlYRg.iFEPPT > div.sc-gCRolh.juzXdM > div.sc-kxZkPw.czOSqk"
+testrun_status = "#result-wrapper > div.sc-iCunDJ.dCBIrN > div > div > div.sc-hXaAvY.dQtrlU > div.sc-hIsoIq.cxofZo > div > div.sc-esPSxW.cbzDGM > div > div > div > div:nth-child(9) > div.sc-iCbVgq.jWTVER > div > div > span"
+
+testrun_result_message = "#result-wrapper > div.sc-iCunDJ.dCBIrN > div > div > div.sc-bJoeBu.fGzRwu > div"
