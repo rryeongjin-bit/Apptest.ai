1ce1d6ebbd95f7d8730a061e39510411199091ed
Yeongjin-heo <ridi.heo@ridi.com>
2025-11-18 16:01:10 +0900

    1118 [prod] 작품홈 작업추가

--- win/test_apptestai/test_contentshome/test_genre_ebook_mid2.py
+++ win/test_apptestai/test_contentshome/test_genre_ebook_mid2.py
@@ -8,33 +8,38 @@ from conftest import *
 # 로그인&계정전환 + 프로젝트 폴더 진입
 # -------------------------------
 @pytest.mark.order(1)
-@pytest.mark.prod_launchapp
-@pytest.mark.stg_launchapp
+@pytest.mark.prod_contentshome
+@pytest.mark.stg_contentshome
 def test_login_enter_project(main_homepage):
     page = main_homepage
     login_and_select_project(page)
 
 # -------------------------------
-# [Prod] 앱실행 프로젝트
+# [Prod] 작품홈 프로젝트
 # -------------------------------
-TCID = [ "App_CheckList_001"]
-
+TCID = [ "App_CheckList_213", "App_CheckList_214", "App_CheckList_216", 
+        "App_CheckList_217", "App_CheckList_218", "App_CheckList_219", 
+        "App_CheckList_221", "App_CheckList_222", "App_CheckList_223", 
+        "App_CheckList_224", "App_CheckList_226", "App_CheckList_227",
+        "App_CheckList_228", "App_CheckList_229", "App_CheckList_426", 
+        "App_CheckList_427", "App_CheckList_428", "App_CheckList_441"]
+        
 @pytest.mark.order(2)
-@pytest.mark.prod_launchapp
-def test_project_launchapp(main_homepage):
+@pytest.mark.prod_contentshome
+def test_project_contentshome(main_homepage):
     page = main_homepage
-    page.click(prod_launchapp)
+    page.click(prod_contentshome)
 
-    target_project = page.locator(project_title).get_by_text("[Prod] 앱실행")
+    target_project = page.locator(project_title).get_by_text("[Prod] 작품홈")
     try:
         target_project.wait_for(state="visible", timeout=5000)
     except TimeoutError:
-        assert False, f"❌ {prod_launchapp} 폴더 진입 실패"
+        assert False, f"❌ {prod_contentshome} 폴더 진입 실패"
 
-    assert target_project.is_visible(), f"❌ {prod_launchapp} 폴더 진입 실패"
+    assert target_project.is_visible(), f"❌ {prod_contentshome} 폴더 진입 실패"
 
 @pytest.mark.order(3)
-@pytest.mark.prod_launchapp
+@pytest.mark.prod_contentshome
 def test_checkresult(main_homepage):
     page = main_homepage
     targets = [
@@ -42,26 +47,30 @@ def test_checkresult(main_homepage):
         ]
 
     click_and_verify(page, btn_test_run, targets)
-    
+    select_rows(page)
+
+"""
+📍 작품 홈_genre-ebook_중간 영역_회차 리스트
+"""    
 @pytest.mark.order(4)
-@pytest.mark.prod_launchapp
+@pytest.mark.prod_contentshome
 def test_checkresult_AOS(main_homepage):
     page = main_homepage
     apply_filter_checkbox_AOS(page)
 
 @pytest.mark.order(5)
-@pytest.mark.prod_launchapp
+@pytest.mark.prod_contentshome
 def test_testrun_info_AOS(main_homepage,aos_flag, sheet):
     page = main_homepage
-    AOS_testrun_launchapp = page.locator(testrun_first).filter(
-        has_text=re.compile(r"앱실행", re.IGNORECASE)
+    AOS_testrun_genre_ebook_mid = page.locator(testrun_first).filter(
+        has_text=re.compile(r"작품 홈_genre-ebook_중간 영역_회차 리스트_AOS$", re.IGNORECASE) 
     ).first
 
     try:
-        AOS_testrun_launchapp.wait_for(state="attatched", timeout=5000)
-        AOS_testrun_launchapp.scroll_into_view_if_needed()
-        AOS_testrun_launchapp.wait_for(state="visible", timeout=5000)
-        AOS_testrun_launchapp.click()
+        AOS_testrun_genre_ebook_mid.wait_for(state="attached", timeout=5000)
+        AOS_testrun_genre_ebook_mid.scroll_into_view_if_needed()
+        AOS_testrun_genre_ebook_mid.wait_for(state="visible", timeout=5000)
+        AOS_testrun_genre_ebook_mid.click()
 
         AOS_testrun_info = get_testrun_info(page, testrun_id_section)
         write_result_by_key(sheet, TCID, AOS_testrun_info, column="S")
@@ -72,40 +81,40 @@ def test_testrun_info_AOS(main_homepage,aos_flag, sheet):
         pytest.skip("⚠️ AOS 테스트 결과 없음 - 테스트 정보 확인 skip")
 
 @pytest.mark.order(6)
-@pytest.mark.prod_launchapp
+@pytest.mark.prod_contentshome
 def test_check_testresult_AOS(main_homepage, aos_flag, sheet):
     if not aos_flag["run"]:
         write_result_by_key(sheet, TCID, "N/T", column="P")
         pytest.skip("⚠️ AOS 테스트 결과 없음 - 결과 확인 skip")
 
     page = main_homepage
-    App_CheckList_001_AOS= get_testrun_status_AOS(page, testrun_status)
-    write_result_by_key(sheet,TCID, App_CheckList_001_AOS, column="P")
+    App_CheckList_213_AOS = get_testrun_status_AOS(page, testrun_status)
+    write_result_by_key(sheet, TCID, App_CheckList_213_AOS, column="P")
 
 @pytest.mark.order(7)
-@pytest.mark.prod_launchapp
+@pytest.mark.prod_contentshome
 def test_back_testrun_list_AOS(main_homepage, aos_flag):
     back_and_or_reset_AOS(main_homepage, aos_flag.get("run", False))
 
 @pytest.mark.order(8)
-@pytest.mark.prod_launchapp
+@pytest.mark.prod_contentshome
 def test_checkresult_IOS(main_homepage):
     page = main_homepage
     apply_filter_checkbox_iOS(page)
 
 @pytest.mark.order(9)
-@pytest.mark.prod_launchapp
+@pytest.mark.prod_contentshome
 def test_testrun_info_IOS(main_homepage, ios_flag, sheet):
     page = main_homepage
-    IOS_testrun_launchapp = page.locator(testrun_first).filter(
-        has_text=re.compile(r"앱실행", re.IGNORECASE)
+    IOS_testrun_genre_ebook_mid = page.locator(testrun_first).filter(
+        has_text=re.compile(r"작품 홈_genre-ebook_중간 영역_회차 리스트_iOS$", re.IGNORECASE) 
     ).first
 
     try:
-        IOS_testrun_launchapp.wait_for(state="attached", timeout=5000)
-        IOS_testrun_launchapp.scroll_into_view_if_needed()
-        IOS_testrun_launchapp.wait_for(state="visible", timeout=5000)
-        IOS_testrun_launchapp.click()
+        IOS_testrun_genre_ebook_mid.wait_for(state="attached", timeout=5000)
+        IOS_testrun_genre_ebook_mid.scroll_into_view_if_needed()
+        IOS_testrun_genre_ebook_mid.wait_for(state="visible", timeout=5000)
+        IOS_testrun_genre_ebook_mid.click()
     
         IOS_testrun_info = get_testrun_info(page, testrun_id_section)
         write_result_by_key(sheet, TCID, IOS_testrun_info, column="T")
@@ -116,24 +125,24 @@ def test_testrun_info_IOS(main_homepage, ios_flag, sheet):
         pytest.skip("⚠️ IOS 테스트 결과 없음 - 테스트 정보 확인 skip")
 
 @pytest.mark.order(10)
-@pytest.mark.prod_launchapp
+@pytest.mark.prod_contentshome
 def test_check_testresult_IOS(main_homepage, ios_flag, sheet):
     if not ios_flag["run"]:
         write_result_by_key(sheet, TCID, "N/T", column="R")
-        pytest.skip("⚠️ IOS 테스트 결과 없음 - 결과 확인 skip")
+        pytest.skip("⚠️ AOS 테스트 결과 없음 - 결과 확인 skip")
 
     page = main_homepage
-    App_CheckList_001_iOS = get_testrun_status_IOS(page, testrun_status)
-    write_result_by_key(sheet,TCID, App_CheckList_001_iOS, column="R")
+    App_CheckList_213_iOS = get_testrun_status_IOS(page, testrun_status)
+    write_result_by_key(sheet,TCID, App_CheckList_213_iOS, column="R")
 
 @pytest.mark.order(11)
-@pytest.mark.prod_launchapp
+@pytest.mark.prod_contentshome
 def test_back_testrun_list_IOS(main_homepage, ios_flag):
     back_and_or_reset_IOS(main_homepage, ios_flag.get("run", False))
 
 
 # -------------------------------
-# ⌛ [Stage] 앱실행 프로젝트 ⌛
+# ⌛ [Stage] 작품홈 프로젝트 ⌛
 # ------------------------------
 
 
@@ -142,10 +151,15 @@ def test_back_testrun_list_IOS(main_homepage, ios_flag):
 # -------------------------------
 
 # 비교할 key 값 리스트
-keys_to_copy = ["App_CheckList_001"]
+keys_to_copy =  [ "App_CheckList_213", "App_CheckList_214", "App_CheckList_216", 
+        "App_CheckList_217", "App_CheckList_218", "App_CheckList_219", 
+        "App_CheckList_221", "App_CheckList_222", "App_CheckList_223", 
+        "App_CheckList_224", "App_CheckList_226", "App_CheckList_227",
+        "App_CheckList_228", "App_CheckList_229", "App_CheckList_426", 
+        "App_CheckList_427", "App_CheckList_428", "App_CheckList_441"]
 
-@pytest.mark.prod_launchapp
-@pytest.mark.stg_launchapp
+@pytest.mark.prod_contentshome
+@pytest.mark.stg_contentshome
 @pytest.mark.order(12)
 def test_copy_cell_if_match(sheet):
     sheet1 = sheet
@@ -165,3 +179,4 @@ def test_copy_cell_if_match(sheet):
             },
             key_value=key
         )
+
